eb1c3e47858272ddebb5dc8d0904da6bbaea7e27
surumi <surumi@archis.company>
2022-04-06 15:03:28 +0530

    rental services initial commit

---
+++ src/main/java/com/archisacademy/rentalservices/RentalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.archisacademy.rentalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archisacademy/rentalservices/RentalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archisacademy.rentalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
