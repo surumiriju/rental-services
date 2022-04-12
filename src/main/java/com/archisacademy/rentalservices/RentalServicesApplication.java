ebead3130290f5135ebd98e5cf256c2fd34d0512
surumi <surumi@archis.company>
2022-04-12 16:12:17 +0530

    updated the spotless version

--- src/main/java/com/archisacademy/rentalservices/RentalServicesApplication.java
+++ src/main/java/com/archisacademy/rentalservices/RentalServicesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentalServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentalServicesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RentalServicesApplication.class, args);
+    }
 }
--- src/main/java/com/archisacademy/rentalservices/controller/WelcomeController.java
+++ src/main/java/com/archisacademy/rentalservices/controller/WelcomeController.java
@@ -6,17 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping(value="/welcome")
-    public String welcometotraining(){
+    @GetMapping(value = "/welcome")
+    public String welcometotraining() {
         return "Hello from Archis";
-
-
     }
-    @GetMapping(value="/hello")
-    public String helloToTraining(){
+
+    @GetMapping(value = "/hello")
+    public String helloToTraining() {
         return "Hello from Surumi";
-
-
     }
-
 }
